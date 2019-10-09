8f6c0cfc017557b08197b41d2e06cc8c43e7a2a9
Zeheng Li <imzehengl@gmail.com>
2019-10-09 08:53:03 -0600

    add example crawl script

---
+++ crawl.ps1
@@ -0,0 +1,7 @@
+$companies = @("City-of-Calgary", "City-of-Edmonton", "City-of-Toronto")
+
+foreach ($company in $companies) {
+    $Env:indeed_company = $company
+    scrapy crawl review -o reviews_$Env:indeed_company.json
+    python eda.py
+}
